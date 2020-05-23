abd56f187a841ce2e835f28c01b0e0cba323b5aa
Niklas Ekström <mail@niklasekstrom.nu>
2020-05-23 20:16:53 +0200

    Updated code to handle current site structure
    Also upgraded the code to work with Python 3, and updated the README.md.

--- README.md
+++ README.md
@@ -8,14 +8,13 @@ Do not try to modify the script to download many documents in parallel; at some 
 
 # Installing and running:
 
-* Download and install the latest release of [Python 2](https://www.python.org/downloads/).
-* Download and install pip: download [this script](https://bootstrap.pypa.io/get-pip.py) and run python get-pip.py at the command line. [More info](https://pip.pypa.io/en/latest/installing.html).
-* Install the [requests](http://docs.python-requests.org/en/latest/) library: run the command pip install requests. [More info](http://docs.python-requests.org/en/latest/user/install/).
+* Download and install the latest release of [Python 3](https://www.python.org/downloads/).
+* Install the [requests](http://docs.python-requests.org/en/latest/) library: run the command pip3 install requests. [More info](http://docs.python-requests.org/en/latest/user/install/).
 
 Run using the command line python acmdownload.py.
 
 # Finding the UID of a paper
 
-Search for a paper on [dl.acm.org](dl.acm.org) and go to its page. For example the page for the paper "Spanner: Google's globally-distributed database" from OSDI'12 has the url [http://dl.acm.org/citation.cfm?id=2387880.2387905&coll=DL&dl=GUIDE&CFID=236350197&CFTOKEN=90827288](http://dl.acm.org/citation.cfm?id=2387880.2387905&coll=DL&dl=GUIDE&CFID=236350197&CFTOKEN=90827288). The UID is the string 2387905, which is the part of the id argument after the dot. If there is no dot then the UID is the entire id argument.
+Search for a paper on [dl.acm.org](dl.acm.org) and go to its page. For example the page for the paper "Spanner: Google's globally-distributed database" from OSDI'12 has the url [https://dl.acm.org/doi/10.5555/2387880.2387905](https://dl.acm.org/doi/10.5555/2387880.2387905). The DOI is the string 10.5555/2387880.2387905.
 
-Change the uid global variable in the script to the uid for your paper. The documents_to_download global variable is the total number of documents to include in the set before ranking them according to number of references from within the set.
+Change the doi global variable in the script to the DOI for your paper. The documents_to_download global variable is the total number of documents to include in the set before ranking them according to number of references from within the set.
--- acmdownload.py
+++ acmdownload.py
@@ -1,4 +1,4 @@
-from HTMLParser import HTMLParser
+from html.parser import HTMLParser
 import json
 import requests
 import sqlite3
@@ -51,37 +51,105 @@ def save_doc(uid, doc):
 class PageParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
-        self.doc = {'references':[], 'citedby':[]}
-        self.section = None
+        self.in_title = False
+        self.in_references_item = False
+        self.refs = []
+        self.cbu = None
+        self.title = None
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'meta':
-            d = dict(attrs)
-            if 'name' in d and d['name'].startswith('citation_'):
-                name = d['name'][9:]
-                content = d['content']
-                self.doc[name] = content
-        elif tag == 'a':
-            d = dict(attrs)
-            if 'name' in d:
-                name = d['name']
-                self.section = name if name in ['references', 'citedby'] else None
-            if self.section is not None and 'href' in d and d['href'].startswith('citation.cfm?'):
-                query = d['href'][13:]
-                for p in query.split('&'):
-                    a = p.split('=')
-                    if a[0] == 'id':
-                        self.doc[self.section].append(a[1])
+        d = dict(attrs)
+        if tag == 'h1' and 'citation__title' in d['class']:
+            self.in_title = True
+        elif tag == 'a' and 'data-ajaxurl' in d:
+            cited_by_url = d['data-ajaxurl']
+            if cited_by_url.startswith('/action/ajaxShowCitedBy'):
+                self.cbu = cited_by_url
+        elif self.in_references_item:
+            if tag == 'a':
+                link = d['href']
+                if link.startswith('https://dl.acm.org/doi/'):
+                    link = link[23:]
+                    self.refs.append(link)
+        else:
+            if tag == 'li' and 'class' in d and 'references__item' in d['class']:
+                self.in_references_item = True
 
-cookies = {}
-def download_doc(uid):
-    global cookies
-    url = 'http://dl.acm.org/citation.cfm?id=' + uid + '&preflayout=flat'
-    r = requests.get(url, cookies = cookies, headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36'})
-    cookies.update(r.cookies)
-    parser = PageParser()
-    parser.feed(r.text)
-    return parser.doc
+    def handle_endtag(self, tag):
+        if self.in_title:
+            self.in_title = False
+        elif self.in_references_item:
+            if tag == 'li':
+                self.in_references_item = False
+
+    def handle_data(self, data):
+        if self.in_title:
+            self.title = data
+
+class CitationParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.links = []
+
+    def handle_starttag(self, tag, attrs):
+        d = dict(attrs)
+        if tag == 'a':
+            link = d['href']
+            if link.startswith('https://doi.org/'):
+                link = link[16:]
+                self.links.append(link)
+
+def download_doc(doi):
+    r = requests.get('https://dl.acm.org/doi/' + doi)
+    page_parser = PageParser()
+    page_parser.feed(r.text)
+
+    doc = {'references': page_parser.refs}
+
+    if page_parser.title:
+        doc['title'] = page_parser.title
+
+    if page_parser.cbu:
+        r = requests.get('https://dl.acm.org' + page_parser.cbu)
+        citation_parser = CitationParser()
+        citation_parser.feed(r.text)
+        doc['citedby'] = citation_parser.links
+    else:
+        doc['citedby'] = []
+
+    r = requests.post('https://dl.acm.org/action/exportCiteProcCitation', data={
+        'dois': doi,
+        'targetFile': 'custom-bibtex',
+        'format': 'bibTex'
+    })
+
+    j = json.loads(r.text)
+    kvs = list(j['items'][0].items())[0][1]
+
+    if 'title' not in doc and 'title' in kvs:
+        doc['title'] = kvs['title']
+
+    for k in ['original-date', 'issued']:
+        if k in kvs:
+            date = kvs[k]
+            ds = map(str, date['date-parts'][0])
+            doc['date'] = '/'.join(ds)
+            break
+
+    if 'author' in kvs:
+        l = []
+        for a in kvs['author']:
+            name = ''
+            if 'given' in a:
+                name += a['given']
+            if 'family' in a:
+                if name:
+                    name += ' '
+                name += a['family']
+            l.append(name)
+        doc['authors'] = ', '.join(l)
+
+    return doc
 
 def get_top_ranked(docs, missing):
     r = {uid:0 for uid in missing}
@@ -109,16 +177,16 @@ def download(orig, num_docs):
         if len(queue) == 0:
             missing = uids - set(docs.keys())
             if len(missing) == 0:
-                print 'No documents are missing, stopping.'
+                print('No documents are missing, stopping.')
                 exit()
             queue |= get_top_ranked(docs, missing)
 
         uid = queue.pop()
         if uid in all_docs:
-            print '%s: Taking %s from store' % (len(docs) + 1, uid)
+            print('%s: Taking %s from store' % (len(docs) + 1, uid))
             doc = all_docs[uid]
         else:
-            print '%s: Downloading %s...' % (len(docs) + 1, uid)
+            print('%s: Downloading %s...' % (len(docs) + 1, uid))
             doc = download_doc(uid)
             all_docs[uid] = doc
             save_doc(uid, doc)
@@ -146,14 +214,14 @@ def mostreferenced(docs, orig):
     for i in range(min(100, len(docs))):
         uid, cnt = rank[i]
         doc = docs[uid]
-        print '%3d, %8s, %3d (%3d), %s. %s: %s' % (i + 1, uid, cnt, len(doc['citedby']), doc['date'] if 'date' in doc else '??/??/????', doc['authors'] if 'authors' in doc else '???', doc['title'] if 'title' in doc else '???')
+        print('%3d, %8s, %3d (%3d), %s. %s: %s' % (i + 1, uid, cnt, len(doc['citedby']), doc['date'] if 'date' in doc else '??/??/????', doc['authors'] if 'authors' in doc else '???', doc['title'] if 'title' in doc else '???'))
 
 def info(doc):
     for (key, value) in doc.items():
         if key not in ['references', 'citedby']:
-            print '%s: %s' % (key, value)
-    print 'references: %s' % len(doc['references'])
-    print 'cited by: %s' % len(doc['citedby'])
+            print('%s: %s' % (key, value))
+    print('references: %s' % len(doc['references']))
+    print('cited by: %s' % len(doc['citedby']))
 
 def remove_uid_file(uid):
     docs = load_docs_file()
@@ -178,10 +246,8 @@ def remove_missing_meta():
             del docs[uid]
     #save_docs(docs)
 
-uid = '2387905'
-documents_to_download = 250
-docs = download(uid, documents_to_download)
-info(docs[uid])
-mostreferenced(docs, uid)
-#remove_uid('2611484')
-#remove_missing_meta()
+doi = '10.5555/2387880.2387905'
+documents_to_download = 300
+docs = download(doi, documents_to_download)
+info(docs[doi])
+mostreferenced(docs, doi)
